9272a4506ac2e18b3324d2301d94178a5b460117
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2020-11-28 13:37:27 -0700

    Improve sandbox_run.ps1

--- runtime_directory/sandbox_run.ps1
+++ runtime_directory/sandbox_run.ps1
@@ -8,26 +8,22 @@
 
 cd $PSScriptRoot
 
-Get-ChildItem -Path "HKLM:\HARDWARE\DESCRIPTION" | Remove-ItemProperty -Name SystemBiosVersion
-
-rm HKLM:\HARDWARE\DESCRIPTION\System\BIOS
-
+$lockdown_extract_dir = "C:\Windows\Temp\Lockdown"
+$lockdown_runtime = "C:\Program Files (x86)\Respondus\LockDown Browser\LockDownBrowser.exe"
 $lockdown_installer = (ls Lockdown*)[0]
 
-$lockdown_runtime = "C:\Program Files (x86)\Respondus\LockDown Browser\LockDownBrowser.exe"
+Get-ChildItem -Path "HKLM:\HARDWARE\DESCRIPTION" | Remove-ItemProperty -Name SystemBiosVersion
+rm HKLM:\HARDWARE\DESCRIPTION\System\BIOS
 
-& $lockdown_installer /x "`"C:\Windows\Temp\Lockdown`"" # Dumb installer needs a quoted path, even with no spaces. Also, we have to extract the program before we can even run a silent install.
-
-sleep 10
-
-kill -Name *Lockdown* # I don't want to script clicking the "Ok" button, so let's just wait 10 seconds and kill the program.
-
-& "C:\Windows\Temp\Lockdown\setup.exe" /s "/f1$PSScriptRoot\setup.iss" "/f2C:\Users\wdagutilityaccount\Desktop\setup.log" # If we don't give a log file path, this doesn't work
-
-while (!(Test-Path $lockdown_runtime)) {
-    sleep 0.25 # Being dumb some more, we can't just wait on the installer to return because it forks, so we just need to watch the files that it installs
+& $lockdown_installer /x "`"$lockdown_extract_dir`"" # Dumb installer needs a quoted path, even with no spaces. Also, we have to extract the program before we can even run a silent install.
+while (!(Test-Path $lockdown_extract_dir\id.txt)) {
+    # This is the easiest way to detect if the installer is finished extracting
+    sleep 0.2
 }
+sleep 1
+kill -Name *Lockdown*
 
-sleep 5
+& "$lockdown_extract_dir\setup.exe" /s "/f1$PSScriptRoot\setup.iss" "/f2$PSScriptRoot\..\setup.log" # If we don't give a log file path, this doesn't work
+Wait-Process -Name "setup"
 
 ./withdll /d:GetSystemMetrics-Hook.dll $lockdown_runtime
