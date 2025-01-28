635d2ce56dd43775c29529111efea209a85204c7
Voidless7125 <167136334+Voidless7125@users.noreply.github.com>
2025-01-28 10:39:32 -0500

    Refactor sandbox_run.ps1: Improve installer handling, enhance logging, and streamline script execution

--- runtime_directory/sandbox_run.ps1
+++ runtime_directory/sandbox_run.ps1
@@ -28,10 +28,10 @@ if ($env:USERNAME -ne "WDAGUtilityAccount") {
 
 Set-Location $PSScriptRoot
 
-$lockdown_extract_dir = [System.Environment]::GetFolderPath("System") + "\Temp\Lockdown"
-$lockdown_installer = Get-ChildItem Lockdown*
-if ($lockdown_installer.Count -ne 1) {
-    Write-Log "Multiple or no Lockdown installers found in the current directory. Exiting..."
+$lockdown_extract_dir = "C:\Windows\Temp\Lockdown"
+$lockdown_installer = Get-ChildItem Lockdown* | Select-Object -First 1
+if (-not $lockdown_installer) {
+    Write-Log "No Lockdown installer found in the current directory. Exiting..."
     exit 1
 }
 elseif ($lockdown_installer.Name -like "LockDownBrowserOEMSetup.exe") {
@@ -43,9 +43,10 @@ else {
 
 function Remove-SystemInfo {
     Write-Log "Removing system information..."
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
     Get-ChildItem -Path "HKLM:\HARDWARE\DESCRIPTION" | Remove-ItemProperty -Name SystemBiosVersion -ErrorAction Ignore
-    Remove-Item -Path [System.Environment]::GetFolderPath("System") + "\BIOS" -ErrorAction Ignore
-    $vmcompute_path = [System.Environment]::GetFolderPath("System") + "\System32\VmComputeAgent.exe"
+    Remove-Item -Path "HKLM:\HARDWARE\DESCRIPTION\System\BIOS" -ErrorAction Ignore
+    $vmcompute_path = [System.Environment]::GetFolderPath("System") + "\VmComputeAgent.exe"
     takeown /f $vmcompute_path
     icacls $vmcompute_path /grant "Everyone:(D)"
     Remove-Item -Path $vmcompute_path -ErrorAction Ignore
@@ -53,8 +54,8 @@ function Remove-SystemInfo {
 }
 
 function Install-LockdownBrowser {
-    Write-Log "Installing Lockdown Browser..."
     if ($lockdown_installer.Name -like "LockDownBrowserOEMSetup.exe") {
+        Write-Log "Installing Lockdown Browser OEM..."
         & $lockdown_installer /s /r
         Write-Log "OEM installer executed."
     }
@@ -63,27 +64,31 @@ function Install-LockdownBrowser {
         # Dumb installer needs a quoted path, even with no spaces. 
         # Also, we have to extract the program before we can even run a silent install.
         Write-Log "Extracting Lockdown Browser..."
-        while (!(Test-Path "$lockdown_extract_dir\id.txt")) {
-            Start-Sleep -Seconds 0.2
-        }
-        Start-Sleep -Seconds 1
-        Stop-Process -Name *Lockdown* -ErrorAction Ignore
-
+        Wait-Process -Name *Lockdown* # For some weird reason, if the extracter gets killed the installer can fail sometimes on missing a file.
+        # You get get a prompt saying it's been extracted, so just click okay.
         if (-not (Test-Path "$lockdown_extract_dir\setup.exe")) {
             Write-Log "Setup file not found after extraction. Exiting..."
             exit 1
         }
-
+        Write-Log "Installing Lockdown Browser..."
         & "$lockdown_extract_dir\setup.exe" /s "/f1$PSScriptRoot\setup.iss" "/f2$PSScriptRoot\..\setup.log"
         Write-Log "Setup executed."
         Wait-Process -Name "setup"
     }
+    # Remove existing shortcut if it exists
+    $public_desktop_path = "C:\Users\Public\Desktop"
+    $shortcut_path = Join-Path -Path $public_desktop_path -ChildPath "LockDown Browser.lnk"
+    if (Test-Path $shortcut_path) {
+        Remove-Item -Path $shortcut_path -Force
+        Write-Log "Removed existing LockDown Browser shortcut from public desktop."
+    }
 }
 
 function Register-URLProtocol {
-    Write-Log "Registering URL protocol..."
+    Write-Log "Registering URL protocol(s)..."
     New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
     if ($lockdown_installer.Name -like "LockDownBrowserOEMSetup.exe") {
+        # I got the urls from installing LDB OEM and looking in the regestery for :Lockdown Broswe OEM and fould all these HKCR keys."
         $urls = @("anst", "cllb", "ibz", "ielb", "jnld", "jzl", "ldb", "ldb1", "pcgs", "plb", "pstg", "rzi", "uwfb", "xmxg")
         $urls | ForEach-Object {
             try {
@@ -109,27 +114,19 @@ function Register-URLProtocol {
 
 function New-RunLockdownBrowserScript {
     Write-Log "Creating run script on desktop..."
-    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
     if ($lockdown_installer.Name -like "LockDownBrowserOEMSetup.exe") {
         $script_content = @"
 # Ask for the URL
 $url = Read-Host -Prompt "Please enter the URL"
 Write-Host "Running Lockdown Browser with URL: $url"
-# Define the lockdown runtime path
-$lockdown_runtime = [System.Environment]::GetFolderPath("ProgramFilesX86") + "Respondus\LockDown Browser OEM\LockDownBrowserOEM.exe"
+# Define the lockdown runtime path (Need to use quoted path, cannot do it with system variables)
+$lockdown_runtime = "C:\Program Files (x86)\Respondus\LockDown Browser OEM\LockDownBrowserOEM.exe"
 # Change directory and run the command
 cd "C:\Users\WDAGUtilityAccount\Desktop\runtime_directory"
 ./withdll /d:GetSystemMetrics-Hook.dll $lockdown_runtime $url
 "@
     }
     else {
-        # Remove existing shortcut if it exists
-        $public_desktop_path = "C:\Users\Public\Desktop"
-        $shortcut_path = Join-Path -Path $public_desktop_path -ChildPath "LockDown Browser.lnk"
-        if (Test-Path $shortcut_path) {
-            Remove-Item -Path $shortcut_path -Force
-            Write-Log "Removed existing LockDown Browser shortcut from public desktop."
-        }
         $script_content = @"
 cd C:\Users\WDAGUtilityAccount\Desktop\runtime_directory\
 .\withdll.exe /d:GetSystemMetrics-Hook.dll $lockdown_runtime
