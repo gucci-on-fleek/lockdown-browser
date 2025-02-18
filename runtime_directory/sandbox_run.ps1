b93a877b9faae5fcc14b48055e5643e5f196e2f9
Dominic Troy <167136334+Voidless7125@users.noreply.github.com>
2025-02-18 17:57:12 -0500

    Fix for win 11 (Readded from 1.4.1)

--- runtime_directory/sandbox_run.ps1
+++ runtime_directory/sandbox_run.ps1
@@ -33,8 +33,8 @@ Set-Location $PSScriptRoot
 function Remove-SystemInfo {
     Write-Log "Removing system information..."
     Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
-    Get-ChildItem -Path "HKLM:\HARDWARE\DESCRIPTION" | Remove-ItemProperty -Name SystemBiosVersion
-    Remove-Item -Path "HKLM:\HARDWARE\DESCRIPTION\System\BIOS"
+    Get-ChildItem -Path "HKLM:\HARDWARE\DESCRIPTION" | Remove-ItemProperty -Name SystemBiosVersion -ErrorAction Ignore
+    Remove-Item -Path "HKLM:\HARDWARE\DESCRIPTION\System\BIOS" -ErrorAction Ignore
     $vmcompute_path = [System.Environment]::GetFolderPath("System") + "\VmComputeAgent.exe"
     takeown /f $vmcompute_path
     icacls $vmcompute_path /grant "Everyone:(D)"
