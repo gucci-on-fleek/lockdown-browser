288c7b68dcae265785f1398a73ae820c548b36a6
Max Chernoff <git@maxchernoff.ca>
2025-02-24 18:12:10 -0700

    Make `launch.c` actually compile.
    Tested on Linux with MinGW and Wine with the following:
    
        x86_64-w64-mingw32-gcc -o launch.exe launch.c -lntdll

--- src/launch.c
+++ src/launch.c
@@ -7,40 +7,66 @@
 #include <winternl.h>
 #include <stdio.h>
 
-#define win32_checked(result)                                           \
-    do                                                                  \
-    {                                                                   \
-        if (!(result))                                                  \
-        {                                                               \
-            printf("Error: %ld (line %d)\n", GetLastError(), __LINE__); \
-            return result;                                              \
-        }                                                               \
+#define win32_checked(result)                                  \
+    do                                                         \
+    {                                                          \
+        if (!(result))                                         \
+        {                                                      \
+            const DWORD error = GetLastError();                \
+            printf("Error: %ld (line %d)\n", error, __LINE__); \
+            print_error_message(error);                        \
+            return result;                                     \
+        }                                                      \
     } while (0)
 
 #define nt_checked(result)                                     \
     do                                                         \
     {                                                          \
-        if (!(result))                                         \
+        if (result)                                            \
         {                                                      \
             printf("Error: %d (line %d)\n", result, __LINE__); \
             return result;                                     \
         }                                                      \
     } while (0)
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+// Based off of https://learn.microsoft.com/en-us/windows/win32/Debug/retrieving-the-last-error-code
+static void print_error_message(DWORD error)
 {
+    LPVOID lpMsgBuf;
+
+    FormatMessageW(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        error,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPWSTR)&lpMsgBuf,
+        0, NULL);
+
+    printf("Message: %ls\n", lpMsgBuf);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR _lpCmdLine, int nCmdShow)
+{
+    (void)hPrevInstance;
+    (void)_lpCmdLine;
+    (void)nCmdShow;
+
     const LPWSTR lpCmdLine = GetCommandLineW();
 
     int pNumArgs;
     const LPWSTR *argv = CommandLineToArgvW(lpCmdLine, &pNumArgs);
-    const LPWSTR lpApplicationName = argv[0];
+    const LPWSTR lpThisApplicationName = argv[0];
+    const LPWSTR lpTargetApplicationName = argv[1];
+    const BOOL quoted = lpCmdLine[0] == L'"';
 
-    const LPWSTR lpNewCmdLine = lpCmdLine + lstrlenW(lpApplicationName) + 1;
+    const LPWSTR lpNewCmdLine = lpCmdLine + lstrlenW(lpThisApplicationName) + (quoted ? 2 : 0) + 1;
 
     STARTUPINFOW lpStartupInfo = {0};
     PROCESS_INFORMATION lpCreateProcessInformation;
     win32_checked(CreateProcessW(
-        lpApplicationName,
+        lpTargetApplicationName,
         lpNewCmdLine,
         NULL,
         NULL,
@@ -76,7 +102,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         sizeof(lpGetProcessEnvironmentBlock),
         NULL));
 
-    win32_checked(ResumeThread(lpCreateProcessInformation.hThread) == -1);
+    win32_checked(ResumeThread(lpCreateProcessInformation.hThread) != -1);
 
     return 0;
 }
